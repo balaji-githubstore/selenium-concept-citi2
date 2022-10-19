c8a960111545c09e17a6813c313c61550c397e44
Administrator <Administrator@172.20.0.152>
2022-10-19 18:15:29 +0530

    added comment

--- src/com/citi/tabs/Demo2.java
+++ src/com/citi/tabs/Demo2.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//multiple tabs
 public class Demo2 {
 
 	public static void main(String[] args) {
